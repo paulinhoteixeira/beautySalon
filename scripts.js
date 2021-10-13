fe9479cb0ad1155a53c4dd0bd0064d28c6689bd0
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-10-12 21:16:13 -0300

    adicionado o html e css da section testimonials e o swiper no js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,40 @@
-// ABRE E FECHA O MENU QUANDO CLICAR NO ICONE  
-const nav = document.querySelector("#header nav")
-const toggle = document.querySelectorAll("nav .toggle")
+// ABRE E FECHA O MENU QUANDO CLICAR NO ICONE
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-for (const element of toggle){
-    element.addEventListener("click", function(){
-        nav.classList.toggle("show")
-    })
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
+// QUANDO CLICAR NUM ITEM DO MENU, ESCONDER O MENU
+const links = document.querySelectorAll("nav ul li a");
 
-// QUANDO CLICAR NUM ITEM DO MENU, ESCONDER O MENU 
-const links = document.querySelectorAll("nav ul li a")
-
-for (const link of links){
-    link.addEventListener("click", function(){
-        nav.classList.remove("show")
-    })
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
-// MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL 
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
+// MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function(){
-    if(window.scrollY >= navHeight){
-        header.classList.add("scroll")
-    }else{
-        header.classList.remove("scroll")
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
+// TESTIMONIALS CAROUSEL SLIDER SWIPER
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+});
