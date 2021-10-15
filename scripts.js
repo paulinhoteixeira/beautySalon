75a7ad75dda7637fc247b4688e327e8a0442f59f
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-10-14 22:33:03 -0300

    js para ativar menu conforme seção que estiver visivel na página

--- scripts.js
+++ scripts.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 // MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
@@ -38,11 +38,11 @@ const swiper = new Swiper(".swiper", {
   mousewheel: true,
   keyboard: true,
   breakpoints: {
-    767:{
+    767: {
       slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
+      setWrapperSize: true,
+    },
+  },
 });
 
 // SCROLLREVEAL: MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PÁGINA
@@ -66,9 +66,8 @@ scrollReveal.reveal(
 
 // BOTÃO VOLTAR PARA O TOPO
 
+const backToTopButton = document.querySelector(".back-to-top");
 function backToTop() {
-  const backToTopButton = document.querySelector(".back-to-top");
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add("show");
   } else {
@@ -76,11 +75,34 @@ function backToTop() {
   }
 }
 
+// MENU ATIVO CONFORME A SEÇÃO VISIVEL NA PÁGINA
+const sections = document.querySelectorAll("main section[id]");
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    }else{
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
 
 // WHEN SCROLL
 window.addEventListener("scroll", function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
-
-// MENU ATIVO CONFORME A SEÇÃO VISIVEL NA PÁGINA 
